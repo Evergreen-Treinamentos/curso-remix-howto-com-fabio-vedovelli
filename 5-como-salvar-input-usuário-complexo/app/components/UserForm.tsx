fe27709d654d5cddca97d1ecb9f5b2d26dae7671
Fábio Vedovelli <vedovelli@gmail.com>
2023-02-11 05:59:13 -0300

    Código final aula

--- 5-como-salvar-input-usuário-complexo/app/components/UserForm.tsx
+++ 5-como-salvar-input-usuário-complexo/app/components/UserForm.tsx
@@ -1,4 +1,11 @@
-export function UserForm() {
+import type { SomeZodObject } from "zod";
+import { Form } from "~/form";
+
+interface UserFormProps {
+  schema: SomeZodObject;
+}
+
+export function UserForm({ schema }: UserFormProps) {
   return (
     <div className="pt-10 sm:mt-0">
       <div className="md:grid md:grid-cols-3 md:gap-6">
@@ -13,7 +20,65 @@ export function UserForm() {
           </div>
         </div>
         <div className="mt-5 md:col-span-2 md:mt-0">
-          <form method="POST">
+          <Form schema={schema}>
+            {({ Field, Errors, Button, register }) => (
+              <>
+                <Field name="name" label="Name">
+                  {({ Label, Errors }) => (
+                    <>
+                      <Label className="block text-sm font-medium text-gray-700" />
+                      <input
+                        {...register("name")}
+                        className="h-8 mt-1 w-full border border-gray-50 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                      />
+                      <Errors className="text-red-500 font-bold text-sm antialiased" />
+                    </>
+                  )}
+                </Field>
+                <Field name="email" label="E-mail">
+                  {({ Label, Errors }) => (
+                    <>
+                      <Label className="block text-sm font-medium text-gray-700" />
+                      <input
+                        {...register("email")}
+                        className="h-8 mt-1 w-full border border-gray-50 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                      />
+                      <Errors className="text-red-500 font-bold text-sm antialiased" />
+                    </>
+                  )}
+                </Field>
+                <Field name="city" label="City">
+                  {({ Label, Errors }) => (
+                    <>
+                      <Label className="block text-sm font-medium text-gray-700" />
+                      <input
+                        {...register("city")}
+                        className="h-8 mt-1 w-full border border-gray-50 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                      />
+                      <Errors className="text-red-500 font-bold text-sm antialiased" />
+                    </>
+                  )}
+                </Field>
+                <Field name="state" label="State">
+                  {({ Label, Errors }) => (
+                    <>
+                      <Label className="block text-sm font-medium text-gray-700" />
+                      <input
+                        {...register("state")}
+                        className="h-8 mt-1 w-full border border-gray-50 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                      />
+                      <Errors className="text-red-500 font-bold text-sm antialiased" />
+                    </>
+                  )}
+                </Field>
+                <Errors className="text-red-500 font-bold text-sm antialiased" />
+                <Button className="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+                  Save
+                </Button>
+              </>
+            )}
+          </Form>
+          {/* <form method="POST">
             <div className="overflow-hidden shadow sm:rounded-md">
               <div className="bg-white px-4 py-5 sm:p-6">
                 <div className="grid grid-cols-6 gap-6">
@@ -91,7 +156,7 @@ export function UserForm() {
                 </button>
               </div>
             </div>
-          </form>
+          </form> */}
         </div>
       </div>
     </div>
--- 5-como-salvar-input-usuário-complexo/app/routes/users/form.tsx
+++ 5-como-salvar-input-usuário-complexo/app/routes/users/form.tsx
@@ -1,28 +1,45 @@
-import type { User } from "@prisma/client";
-import type { ActionArgs } from "@remix-run/node";
-import { redirect } from "@remix-run/node";
 import { UserForm } from "~/components/UserForm";
+
+import type { ActionArgs } from "@remix-run/node";
+import { makeDomainFunction } from "domain-functions";
+import { z } from "zod";
 import { db } from "~/db.server";
+import { formAction } from "~/form-action.server"; /* path to your custom formAction */
 
-export async function action({ request }: ActionArgs) {
-  // throw new Error("Yeah Mr. White! Yeah SCIENCE!!");
-  const data = Object.fromEntries(await request.formData());
+const schema = z.object({
+  name: z.string().min(1, { message: "Please provide your name" }).trim(),
+  email: z
+    .string()
+    .min(1, { message: "Please provide your email" })
+    .email({ message: "Please provide a valid email" })
+    .trim(),
+  city: z.string().min(1, { message: "Please provide your city" }).trim(),
+  state: z.string().min(1, { message: "Please provide your state" }).trim(),
+});
 
-  await db.user.create({
-    data: data as unknown as User,
+const mutation = makeDomainFunction(schema)(
+  async (data) => await db.user.create({ data })
+);
+
+export const action = async ({ request }: ActionArgs) =>
+  formAction({
+    request,
+    schema,
+    mutation,
+    successPath: "/users",
   });
 
-  return redirect("/users");
-}
-
 export default function () {
-  return <UserForm />;
+  return <UserForm schema={schema} />;
 }
 
-export function ErrorBoundary() {
+export function ErrorBoundary({ error }: { error: Error }) {
   return (
-    <div className="bg-red-100 border border-red-500 p-12 text-red-500 font-bold text-2xl">
-      Something went wrong
+    <div className="bg-red-100 border border-red-500 p-12">
+      <span className=" text-red-500 font-bold text-2xl">
+        Something went wrong
+      </span>
+      {/* <p>{error.message}</p> */}
     </div>
   );
 }
